c6708a7fb778d6b5e3d188cf8f09071f648c8582
timur <tmirzabekov@internet.ru>
2021-11-05 18:47:31 +0300

    wrote a programe to check if number is even or odd

---
+++ WeirdOrNot.py
@@ -0,0 +1,13 @@
+# fiel:WeirdOrNot.py 
+# author:Timur Mirzabekov
+# versian 1005
+
+#ask for input
+number=input(" enter a number ")
+
+# if remainder is zero the number is even
+if int(number) % 2==0:
+    print(" not weird ")
+
+else: 
+    print(" weird ")
